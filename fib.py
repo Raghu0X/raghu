f8141db42a2e295c0a3100caa9a314ac93df5f13
Raghu0X <raghuburud9@gmail.com>
2025-03-12 15:10:08 +0530

    Create fib.py

---
+++ fib.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib_sequence = [0, 1]
+    for _ in range(2, n):
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    
+    return fib_sequence
+
+print(fibonacci(10))  

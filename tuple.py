fce943a1345a80e874965f2625f05ac8e78bea70
Raghu0X <raghuburud9@gmail.com>
2025-03-13 14:59:33 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,14 @@
+my_tuple = (10, 20, 30, 40, 50)
+
+first_element = my_tuple[0]
+second_element = my_tuple[1]
+third_element = my_tuple[2]
+fourth_element = my_tuple[3]
+fifth_element = my_tuple[4]
+
+print("First element:", first_element)
+print("Second element:", second_element)
+print("Third element:", third_element)
+print("Fourth element:", fourth_element)
+print("Fifth element:", fifth_element)
+   

5ad878b6ba7df1b9af63e600db3753906302168e
Raghu0X <raghuburud9@gmail.com>
2025-03-13 15:19:24 +0530

    Create mydict.py

---
+++ mydict.py
@@ -0,0 +1,11 @@
+my_dict = {
+    "name": "raghu",
+    "age": 18,
+    "city": "athani"
+}
+name = my_dict["name"]
+age = my_dict["age"]
+city = my_dict["city"]
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
